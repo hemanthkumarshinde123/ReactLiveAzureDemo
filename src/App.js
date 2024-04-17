d8b582190eea0ec827cbf1e1b15ce102f90697fe
hemanthkumarshinde123 <sallasreehemanth@gmail.com>
2024-04-17 23:40:11 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to LEAP CODES.
+          WELCOME TO DEMO.
         </p>
         <a
           className="App-link"

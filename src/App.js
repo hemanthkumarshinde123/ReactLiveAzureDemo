f9e56f1f616a80fb8c4a12388559c8021ae0a3a2
hemanthkumarshinde123 <sallasreehemanth@gmail.com>
2024-04-18 12:17:36 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          WELCOME TO DEMO.
+          WELCOME TO LEPACODES.
         </p>
         <a
           className="App-link"
